8ea11491e5d3482adc68137b18e71af1c257fe74
pinch24 <pinch24@gmail.com>
2024-05-06 00:17:32 +0900

    Upload

---
+++ Books/Springboot3 Backend, 2024/IdeaProjects/springboot-developer/src/main/java/io/roughgears/springboot3/springbootdeveloper/MemberRepository.java
@@ -0,0 +1,8 @@
+package io.roughgears.springboot3.springbootdeveloper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
