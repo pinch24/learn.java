90aaca606f02c6c577329678dad7d9ad624d2b90
mk <pinch24@gmail.com>
2024-06-10 09:10:52 +0900

    SB3 - Blog Post, Get API

--- Books/Springboot3 Backend, 2024/IdeaProjects/springboot-developer/src/main/java/io/roughgears/springboot3/springbootdeveloper/controller/BlogApiController.java
+++ Books/Springboot3 Backend, 2024/IdeaProjects/springboot-developer/src/main/java/io/roughgears/springboot3/springbootdeveloper/controller/BlogApiController.java
@@ -2,13 +2,17 @@ package io.roughgears.springboot3.springbootdeveloper.controller;
 
 import io.roughgears.springboot3.springbootdeveloper.domain.Article;
 import io.roughgears.springboot3.springbootdeveloper.dto.AddArticleRequest;
+import io.roughgears.springboot3.springbootdeveloper.dto.ArticleResponse;
 import io.roughgears.springboot3.springbootdeveloper.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,5 +24,16 @@ public class BlogApiController {
         Article saveArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(saveArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
---
+++ Books/Springboot3 Backend, 2024/IdeaProjects/springboot-developer/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO article (title, content) VALUES ('Title 1', 'Content 1')
+INSERT INTO article (title, content) VALUES ('Title 2', 'Content 2')
+INSERT INTO article (title, content) VALUES ('Title 3', 'Content 3')
